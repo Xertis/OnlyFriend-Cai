fcf8d229c45741f6b824a082740a1034725eaf8b
Fantasy2mind <Makskozlov2fantasy@gmail.com>
2025-05-05 20:28:53 +0300

    .

--- src/constants.py
+++ src/constants.py
@@ -10,9 +10,11 @@ class Meta(type):
     def __getitem__(self, key):
         # Позволяет получать значения по ключу, как в словаре.
         if key == "TOKEN":
-            return self.TOKEN
+            return self._TOKEN
         elif key == "NN_MODEL":
-            return self.NN_MODEL
+            return self._NN_MODEL
+        elif key == "DB_PATH":
+            return self._DB_PATH
         else:
             raise KeyError(f"'{key}' не найдено")
 
@@ -21,7 +23,8 @@ class Structure:
     def __init__(self):
         self._values = {
             'TOKEN': os.getenv('TOKEN'),
-            'NN_MODEL': os.getenv('NN_MODEL')
+            'NN_MODEL': os.getenv('NN_MODEL'),
+            'DB_PATH': os.getenv('DB_PATH')
         }
 
     def get(self, key):
@@ -35,13 +38,15 @@ class Structure:
 
 class Constants(metaclass=Meta):
     TOKEN = Structure().get('TOKEN')
-    NN_MODEL = Structure().get('NN_MODEL0')
-    _immutable_attributes = ['_TOKEN', '_NN_MODEL']
+    NN_MODEL = Structure().get('NN_MODEL')
+    DB_PATH = Structure().get('DB_PATH')
+    _immutable_attributes = ['TOKEN', 'NN_MODEL', 'DB_PATH']
 
     def __init__(self):
         self.structure = Structure()
         self._TOKEN = self.structure.get('TOKEN')
         self._NN_MODEL = self.structure.get('NN_MODEL')
+        self._DB_PATH = self.structure.get('DB_PATH')
 
     def __setattr__(self, key, value):
         # Запрещаем изменение существующих неизменяемых атрибутов
@@ -57,6 +62,10 @@ class Constants(metaclass=Meta):
         # Возвращает переменную окружения NN_MODEL.
         return self._NN_MODEL
 
+    def get_db_path(self):
+        # Возвращает переменную окружения DB_PATH.
+        return self._DB_PATH
+
     def set_token(self, value):
         # Устанавливает новое значение для TOKEN в структуре.
         self.structure.set('TOKEN', value)
@@ -64,6 +73,10 @@ class Constants(metaclass=Meta):
     def set_nn_model(self, value):
         # Устанавливает новое значение для NN_MODEL в структуре.
         self.structure.set('NN_MODEL', value)
+
+    def set_db_path(self, value):
+        # Устанавливает новое значение для DB_PATH в структуре.
+        self.structure.set('DB_PATH', value)
 
 
 for key, val in os.environ.items():
